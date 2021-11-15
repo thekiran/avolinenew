8aeb946cdde16abfd62aa6730f0b6abc6cb47663
kiran <kirankumarka43@gmail.com>
2021-11-15 22:52:18 +0530

    dynamic recaptcha testing

--- bundles/js/recaptcha.js
+++ bundles/js/recaptcha.js
@@ -60,59 +60,59 @@ function recaptchaExpiredPopup() {
 // Latest Way Ends
 
 
-$.getScript("js/recaptchaConfiguration.js", function() {});
-
-$.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
-
-var html_element_popup_id, html_element_index_id, onloadCallback;
-
-// $(document).ready(function() {
-    $(function() {
-    $('body').append("<div id='modalContent'></div>");
-    $('body').append("<div id='html_element_popup'></div>");
-    $("#modalContent").load("modal-form.html", function(){
-
-        onloadCallback = function() {
-            html_element_popup_id = grecaptcha.render('html_element_popup', {
-                'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
-                'callback' : recaptchaCallbackPopup,
-                'expired-callback' : recaptchaExpiredPopup
-            });
-            if($('#html_element_index').length) {
-                console.log("Hereeeehtml_element_index");
-                html_element_index_id = grecaptcha.render('html_element_index', {
-                    'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
-                    'callback' : recaptchaCallbackIndex,
-                    'expired-callback' : recaptchaExpiredIndex
-                });
-            }
-        };
-        window.onloadCallback = onloadCallback
+        $.getScript("js/recaptchaConfiguration.js", function() {});
 
         $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
-        // $.getScript("js/recaptchaConfiguration.js", function() {});
-        $.getScript("js/moment-with-locales.min.js", function() {
-            $.getScript("js/bootstrap-datetimepicker.min.js", function() {
-                $('#form_datetime').datetimepicker({
-                    // defaultDate: new Date(),
-                    minDate: new Date(),
-                    ignoreReadonly: true,
-                    stepping: 30,
-                    enabledHours: [15, 16, 17, 18, 19, 20],
-                    // inline: true,
-                    icons: {
-                        time: 'cstm-sel cstm-sel-time',
-                        date: 'cstm-sel cstm-sel-date'
-                    },
-                    // "setDate": new Date(),
+
+        var html_element_popup_id, html_element_index_id, onloadCallback;
+
+        
+        // $(document).ready(function() {
+
+            $('body').append("<div id='modalContent'></div>");
+            $('body').append("<div id='html_element_popup'></div>");
+            // $("#modalContent").load("modal-form.html", function(){
+            // });
+
+                onloadCallback = function() {
+                    html_element_popup_id = grecaptcha.render('html_element_popup', {
+                        'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
+                        'callback' : recaptchaCallbackPopup,
+                        'expired-callback' : recaptchaExpiredPopup
+                    });
+                    if($('#html_element_index').length) {
+                        console.log("Hereeeehtml_element_index");
+                        html_element_index_id = grecaptcha.render('html_element_index', {
+                            'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
+                            'callback' : recaptchaCallbackIndex,
+                            'expired-callback' : recaptchaExpiredIndex
+                        });
+                    }
+                };
+                window.onloadCallback = onloadCallback
+
+                $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
+                // $.getScript("js/recaptchaConfiguration.js", function() {});
+                $.getScript("js/moment-with-locales.min.js", function() {
+                    $.getScript("js/bootstrap-datetimepicker.min.js", function() {
+                        $('#form_datetime').datetimepicker({
+                            // defaultDate: new Date(),
+                            minDate: new Date(),
+                            ignoreReadonly: true,
+                            stepping: 30,
+                            enabledHours: [15, 16, 17, 18, 19, 20],
+                            // inline: true,
+                            icons: {
+                                time: 'cstm-sel cstm-sel-time',
+                                date: 'cstm-sel cstm-sel-date'
+                            },
+                            // "setDate": new Date(),
+                        });
+                    });
+
                 });
-            });
 
-        });
-
-    });
     
-});
 
 // window.location.href = window.location.origin + '/#modal-popup2';
 
