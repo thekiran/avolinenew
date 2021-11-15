2a88a26e8f8509e2a8ab89dee6560823fb35a50a
kiran <kirankumarka43@gmail.com>
2021-11-15 19:12:20 +0530

    dynamic recaptcha testing

--- bundles/js/newmain.js
+++ bundles/js/newmain.js
@@ -2885,118 +2885,10 @@ $(window).load(function() {
  END Page Load
  ====================================== */
 
-// Latest Way
-function recaptchaCallbackIndex() {
-    $.ajax({
-        url: "./libs/checkCaptcha.php",
-        type: "POST",
-        async: false,
-        data: {
-            // captcha: grecaptcha.getResponse()
-            captcha: grecaptcha.getResponse(html_element_index_id)
-        },
-        success: function(resp) {
-            if (resp === "true") {
-                $('#crcIndex').removeClass('cstm-red-brdr');
-                $('#grErrorMsg').hide();
-                $('#indexForm').val(1);
-            } else {
-                console.log("else11");
-                grecaptcha.reset(html_element_index_id);
-                $('#crcIndex').addClass('cstm-red-brdr');
-                $('#grErrorMsg').show();
-                $('#indexForm').val(0);
-            }
-        }
-    });
-}
 
-function recaptchaExpiredIndex() {
-    $('#indexForm').val(0);
-}
-
-function recaptchaCallbackPopup() {
-    $.ajax({
-        url: "./libs/checkCaptcha.php",
-        type: "POST",
-        async: false,
-        data: {
-            captcha: grecaptcha.getResponse(html_element_popup_id)
-        },
-        success: function(resp) {
-            if (resp === "true") {
-                $('#crcPopup').removeClass('cstm-red-brdr');
-                $('#grErrorMsgPopup').hide();
-                $('#popupForm').val(1);
-            } else {
-                console.log("else22");
-                grecaptcha.reset(html_element_popup_id);
-                $('#crcPopup').addClass('cstm-red-brdr');
-                $('#grErrorMsgPopup').show();
-                $('#popupForm').val(0);
-            }
-        }
-    });
-}
-
-function recaptchaExpiredPopup() {
-    $('#popupForm').val(0);
-}
-
-// Latest Way Ends
-
-
-// $.getScript("js/recaptchaConfiguration.js", function() {});
-
-// $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
-
-var html_element_popup_id, html_element_index_id, onloadCallback;
-
-// $(document).ready(function() {
+// Add JS for Date Time picker
     $(function() {
-    $('body').append("<div id='modalContent'></div>");
-    $('body').append("<div id='html_element_popup'></div>");
-    $("#modalContent").load("modal-form.html", function(){
 
-        onloadCallback = function() {
-            html_element_popup_id = grecaptcha.render('html_element_popup', {
-                'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
-                'callback' : recaptchaCallbackPopup,
-                'expired-callback' : recaptchaExpiredPopup
-            });
-            if($('#html_element_index').length) {
-                console.log("Hereeeehtml_element_index");
-                html_element_index_id = grecaptcha.render('html_element_index', {
-                    'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
-                    'callback' : recaptchaCallbackIndex,
-                    'expired-callback' : recaptchaExpiredIndex
-                });
-            }
-        };
-        window.onloadCallback = onloadCallback
-
-        $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
-        // $.getScript("js/recaptchaConfiguration.js", function() {});
-        $.getScript("js/moment-with-locales.min.js", function() {
-            $.getScript("js/bootstrap-datetimepicker.min.js", function() {
-                $('#form_datetime').datetimepicker({
-                    // defaultDate: new Date(),
-                    minDate: new Date(),
-                    ignoreReadonly: true,
-                    stepping: 30,
-                    enabledHours: [15, 16, 17, 18, 19, 20],
-                    // inline: true,
-                    icons: {
-                        time: 'cstm-sel cstm-sel-time',
-                        date: 'cstm-sel cstm-sel-date'
-                    },
-                    // "setDate": new Date(),
-                });
-            });
-
-        });
-        // Add JS for Date Time picker
-    });
     $("a[title|='Programeaza si Plateste Consultatia Juridica Online']").addClass('popup-with-zoom-anim').attr('href', '#modal-popup2');
     $('.popup-with-zoom-anim').magnificPopup({
         type: 'inline',
@@ -3031,3 +2923,125 @@ var html_element_popup_id, html_element_index_id, onloadCallback;
     // });
 });
 // Add JS for Date Time picker Ends
+
+
+
+
+
+// // Latest Way
+// function recaptchaCallbackIndex() {
+//     $.ajax({
+//         url: "./libs/checkCaptcha.php",
+//         type: "POST",
+//         async: false,
+//         data: {
+//             // captcha: grecaptcha.getResponse()
+//             captcha: grecaptcha.getResponse(html_element_index_id)
+//         },
+//         success: function(resp) {
+//             if (resp === "true") {
+//                 $('#crcIndex').removeClass('cstm-red-brdr');
+//                 $('#grErrorMsg').hide();
+//                 $('#indexForm').val(1);
+//             } else {
+//                 console.log("else11");
+//                 grecaptcha.reset(html_element_index_id);
+//                 $('#crcIndex').addClass('cstm-red-brdr');
+//                 $('#grErrorMsg').show();
+//                 $('#indexForm').val(0);
+//             }
+//         }
+//     });
+// }
+
+// function recaptchaExpiredIndex() {
+//     $('#indexForm').val(0);
+// }
+
+// function recaptchaCallbackPopup() {
+//     $.ajax({
+//         url: "./libs/checkCaptcha.php",
+//         type: "POST",
+//         async: false,
+//         data: {
+//             captcha: grecaptcha.getResponse(html_element_popup_id)
+//         },
+//         success: function(resp) {
+//             if (resp === "true") {
+//                 $('#crcPopup').removeClass('cstm-red-brdr');
+//                 $('#grErrorMsgPopup').hide();
+//                 $('#popupForm').val(1);
+//             } else {
+//                 console.log("else22");
+//                 grecaptcha.reset(html_element_popup_id);
+//                 $('#crcPopup').addClass('cstm-red-brdr');
+//                 $('#grErrorMsgPopup').show();
+//                 $('#popupForm').val(0);
+//             }
+//         }
+//     });
+// }
+
+// function recaptchaExpiredPopup() {
+//     $('#popupForm').val(0);
+// }
+
+// // Latest Way Ends
+
+
+// // $.getScript("js/recaptchaConfiguration.js", function() {});
+
+// // $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
+
+// var html_element_popup_id, html_element_index_id, onloadCallback;
+
+// // $(document).ready(function() {
+//     $(function() {
+//     $('body').append("<div id='modalContent'></div>");
+//     $('body').append("<div id='html_element_popup'></div>");
+//     $("#modalContent").load("modal-form.html", function(){
+
+//         onloadCallback = function() {
+//             html_element_popup_id = grecaptcha.render('html_element_popup', {
+//                 'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
+//                 'callback' : recaptchaCallbackPopup,
+//                 'expired-callback' : recaptchaExpiredPopup
+//             });
+//             if($('#html_element_index').length) {
+//                 console.log("Hereeeehtml_element_index");
+//                 html_element_index_id = grecaptcha.render('html_element_index', {
+//                     'sitekey' : '6Ldvx24aAAAAAFyJHAafuwuTQTkJer02u-TEGj8m',
+//                     'callback' : recaptchaCallbackIndex,
+//                     'expired-callback' : recaptchaExpiredIndex
+//                 });
+//             }
+//         };
+//         window.onloadCallback = onloadCallback
+
+//         $.getScript("https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit", function() {});
+//         // $.getScript("js/recaptchaConfiguration.js", function() {});
+//         $.getScript("js/moment-with-locales.min.js", function() {
+//             $.getScript("js/bootstrap-datetimepicker.min.js", function() {
+//                 $('#form_datetime').datetimepicker({
+//                     // defaultDate: new Date(),
+//                     minDate: new Date(),
+//                     ignoreReadonly: true,
+//                     stepping: 30,
+//                     enabledHours: [15, 16, 17, 18, 19, 20],
+//                     // inline: true,
+//                     icons: {
+//                         time: 'cstm-sel cstm-sel-time',
+//                         date: 'cstm-sel cstm-sel-date'
+//                     },
+//                     // "setDate": new Date(),
+//                 });
+//             });
+
+//         });
+
+//     });
+    
+// });
+
+
+
