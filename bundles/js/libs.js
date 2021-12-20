d75f66005b5df628cb11be43deaf31c71372fdc4
kiran <kirankumarka43@gmail.com>
2021-12-20 17:52:47 +0530

    observer bug fix

--- bundles/js/libs.js
+++ bundles/js/libs.js
@@ -320,7 +320,11 @@ window.onload = function() {
                         TriggerObserver.unobserve(tr);
                         }
                     });
-                },options);
+                },{
+                    root: null,
+                    rootMargin: "0px 0px 0px 0px",
+                    threshold: 0.1,
+                  });
     
                 TriggerObserver.observe(tr);
         
